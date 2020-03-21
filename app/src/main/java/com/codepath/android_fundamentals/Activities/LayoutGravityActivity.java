a73b5562a3f5a9163e7fe8e38e43d2bcd385af0a
Slava Panasenko <usverster@gmail.com>
2020-03-20 21:59:43 -0700

    Update to AndroidX

--- app/src/main/java/com/codepath/android_fundamentals/Activities/LayoutGravityActivity.java
+++ app/src/main/java/com/codepath/android_fundamentals/Activities/LayoutGravityActivity.java
@@ -1,7 +1,8 @@
 package com.codepath.android_fundamentals.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.codepath.android_fundamentals.R;
 
